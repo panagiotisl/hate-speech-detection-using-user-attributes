9b703ae66ff1939b13eb60a30b298b8cabefb3fa
George Apostolopoulos <g.c.apostolopoulos@gmail.com>
2020-09-10 16:33:01 +0300

    update final training dataset

--- extract_final_training_data.py
+++ extract_final_training_data.py
@@ -1,6 +1,5 @@
 import tweepy
 import csv
-import json
 from configuration import log_config, twitter_login_config
 
 logger = log_config.logger
@@ -19,22 +18,30 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 with open(dataset_file, 'r') as read_file, open(tweet_data_file, "w", encoding="utf-8") as write_file:
     lineReader = csv.reader(read_file, delimiter=',')
     fieldnames = [
-        "created_at", "tweet_id", "tweet_text", "user_name", "tweet_retweets", "user_name", "user_descr",
+        "created_at", "tweet_id", "tweet_text", "tweet_hashtags", "tweet_retweets", "tweet_reply_to_user",
+        "user_id", "user_name", "user_screen_name", "user_descr",
         "user_following", "user_followers", "user_location", "user_total_tweets", "user_created_at", "category"
         ]
     writer = csv.DictWriter(write_file, delimiter=',', fieldnames=fieldnames, quoting=csv.QUOTE_NONNUMERIC)
     for row in lineReader:
         try:
             tweet = api.get_status(row[0])
+            hashtags = []
+            for hashtag in tweet.entities['hashtags']:
+                hashtags.append(hashtag['text'])
             writer.writerow({"created_at": tweet.created_at,
                              "tweet_id": tweet.id,
                              "tweet_text": tweet.text,
+                             "tweet_hashtags": hashtags,
                              "tweet_retweets": tweet.retweet_count,
+                             "tweet_reply_to_user": tweet.in_reply_to_user_id,
+                             "user_id": tweet.user.id,
                              "user_name": tweet.user.name,
+                             "user_screen_name": tweet.user.screen_name,
                              "user_descr": tweet.user.description,
+                             "user_location": tweet.user.location,
                              "user_following": tweet.user.friends_count,
                              "user_followers": tweet.user.followers_count,
-                             "user_location": tweet.user.location,
                              "user_total_tweets": tweet.user.statuses_count,
                              "user_created_at": tweet.user.created_at,
                              "category": row[0]
