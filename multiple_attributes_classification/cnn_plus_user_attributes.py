e38e772f9a601b6d0fb353826233d7cba503a343
George Apostolopoulos <g.c.apostolopoulos@gmail.com>
2021-01-22 17:49:07 +0200

    final: combined classification

--- multiple_attributes_classification/cnn_plus_user_attributes.py
+++ multiple_attributes_classification/cnn_plus_user_attributes.py
@@ -8,6 +8,8 @@ from glove_producer import produce_glove_vector_matrix
 from tensorflow.keras.layers import Embedding, Dense, Input, Conv1D, MaxPooling1D, Flatten, Concatenate
 from tensorflow.keras.models import Model
 
+from matplotlib import pyplot as plt
+
 # the following two lines are used to create the plot of the neural network model
 import os
 os.environ["PATH"] += os.pathsep + 'C:\\Program Files\\Graphviz\\bin'
@@ -15,27 +17,28 @@ os.environ["PATH"] += os.pathsep + 'C:\\Program Files\\Graphviz\\bin'
 # GLOVE VECTORS
 word_vector_matrix = produce_glove_vector_matrix(clf_config.EMBEDDING_DIM, data.vocab_size, data.token)
 
-# MODELS
+# MODEL 1
 input_1 = Input(shape=(data.max_length,))
-input_2 = Input(shape=(data.user_attributes_count,))
-
-# get the output from the first submodel
 embedding_layer = Embedding(data.vocab_size, clf_config.EMBEDDING_DIM, weights=[word_vector_matrix])(input_1)
-conv1d_layer = Conv1D(filters=64, kernel_size=5, activation="relu")(embedding_layer)
-max_pooling_layer = MaxPooling1D(pool_size=5)(conv1d_layer)
+conv1d_layer = Conv1D(filters=64, kernel_size=10, activation="relu")(embedding_layer)
+max_pooling_layer = MaxPooling1D()(conv1d_layer)
 flatten_layer = Flatten()(max_pooling_layer)
-dense_text_layer = Dense(8, activation="relu")(flatten_layer)
+dense_text_layer = Dense(32, activation="sigmoid")(flatten_layer)
 
-# get the output from the second submodel
-dense_layer_1 = Dense(8, activation='sigmoid')(input_2)
-dense_layer_2 = Dense(8, activation='sigmoid')(dense_layer_1)
+# MODEL 2
+input_2 = Input(shape=(data.user_attributes_count,))
+dense_layer_1 = Dense(128, activation='sigmoid')(input_2)
+dense_layer_2 = Dense(128, activation='sigmoid')(dense_layer_1)
+dense_layer_3 = Dense(128, activation='sigmoid')(dense_layer_2)
+dense_user_layer = Dense(128, activation='sigmoid')(dense_layer_3)
 
 # concatenate the two outputs
-concat_layer = Concatenate()([dense_text_layer, dense_layer_2])
+concat_layer = Concatenate()([dense_text_layer, dense_user_layer])
 
 # construct output layer
-dense_layer_3 = Dense(4, activation="sigmoid")(concat_layer)
-output = Dense(1, activation="sigmoid")(dense_layer_3)
+concatenate_dense_layer_1 = Dense(128, activation="sigmoid")(concat_layer)
+concatenate_dense_layer_2 = Dense(128, activation="sigmoid")(concatenate_dense_layer_1)
+output = Dense(1, activation="sigmoid")(concatenate_dense_layer_2)
 
 # build final model
 model = Model(inputs=[input_1, input_2], outputs=output)
@@ -44,11 +47,34 @@ model = Model(inputs=[input_1, input_2], outputs=output)
 model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=['acc'])
 
 # plot the model
-plot_model(model, to_file='../nn_plots/cnn_plus_user_attr/model_plot.png', show_shapes=True, show_layer_names=True)
+# plot_model(model, to_file='../nn_plots/cnn_plus_user_attr/model_plot.png', show_shapes=True, show_layer_names=True)
 
-# train the model
-model.fit(x=[data.X_train_text, data.X_train_user], y=data.y_train, epochs=clf_config.EPOCHS, validation_split=clf_config.VALIDATION_SIZE)
+for i in range(1, 4):
 
-# evaluate the model on the test dataset
-loss, accuracy = model.evaluate(x=[data.X_test_text, data.X_test_user], y=data.y_test, verbose=0)
-print('Accuracy: %f' % (accuracy*100))
+    # train the model
+    history = model.fit(x=[data.X_train_text, data.X_train_user], verbose=0, y=data.y_train, epochs=clf_config.EPOCHS, validation_split=clf_config.VALIDATION_SIZE)
+
+    epochs = range(1, clf_config.EPOCHS+1)
+
+    # ACCURACY
+    # plt.plot(epochs, history.history['acc'])
+    # plt.plot(epochs, history.history['val_acc'])
+    # plt.title('Training and Validation Accuracy')
+    # plt.ylabel('accuracy')
+    # plt.xlabel('epochs')
+    # plt.legend(['train', 'val'], loc='lower right')
+    # plt.savefig('cnn_plus_user_accuracy.png')
+
+    # LOSS
+    # plt.plot(epochs, history.history['loss'], 'g')
+    # plt.plot(epochs, history.history['val_loss'], 'b')
+    # plt.title('Training and Validation Loss')
+    # plt.ylabel('loss')
+    # plt.xlabel('epochs')
+    # plt.legend(['train', 'val'], loc='upper left')
+    # plt.savefig('cnn_plus_user_loss.png')
+
+    # evaluate the model on the test dataset
+    loss, accuracy = model.evaluate(x=[data.X_test_text, data.X_test_user], y=data.y_test, verbose=0)
+    print('Accuracy: %f' % (accuracy*100))
+    # print('Loss: %f' % (loss*100))
--- multiple_attributes_classification/lstm_plus_user_attributes.py
+++ multiple_attributes_classification/lstm_plus_user_attributes.py
@@ -5,8 +5,9 @@ import classification_config as clf_config
 import prepare_data as data
 from glove_producer import produce_glove_vector_matrix
 
-from tensorflow.keras.layers import Embedding, Dense, Input, LSTM, Concatenate
+from tensorflow.keras.layers import Embedding, Dense, Input, LSTM, Concatenate, Bidirectional
 from tensorflow.keras.models import Model
+from matplotlib import pyplot as plt
 
 # the following two lines are used to create the plot of the neural network model
 import os
@@ -15,24 +16,26 @@ os.environ["PATH"] += os.pathsep + 'C:\\Program Files\\Graphviz\\bin'
 # GLOVE VECTORS
 word_vector_matrix = produce_glove_vector_matrix(clf_config.EMBEDDING_DIM, data.vocab_size, data.token)
 
-# MODELS
+# MODEL 1
 input_1 = Input(shape=(data.max_length,))
-input_2 = Input(shape=(data.user_attributes_count,))
-
-# get the output from the first submodel
 embedding_layer = Embedding(data.vocab_size, clf_config.EMBEDDING_DIM, weights=[word_vector_matrix])(input_1)
-LSTM_layer_1 = LSTM(8, dropout=0.2, recurrent_dropout=0.2)(embedding_layer)
+LSTM_layer = Bidirectional(LSTM(100, dropout=0.5))(embedding_layer)
+dense_text_layer = Dense(40, activation="sigmoid")(LSTM_layer)
 
-# get the output from the second submodel
-dense_layer_1 = Dense(8, activation='sigmoid')(input_2)
-dense_layer_2 = Dense(8, activation='sigmoid')(dense_layer_1)
+# MODEL 2
+input_2 = Input(shape=(data.user_attributes_count,))
+dense_layer_1 = Dense(200, activation='sigmoid')(input_2)
+dense_layer_2 = Dense(200, activation='sigmoid')(dense_layer_1)
+dense_layer_3 = Dense(200, activation='sigmoid')(dense_layer_2)
+dense_user_layer = Dense(200, activation='sigmoid')(dense_layer_3)
 
 # concatenate the two outputs
-concat_layer = Concatenate()([LSTM_layer_1, dense_layer_2])
+concat_layer = Concatenate()([dense_text_layer, dense_user_layer])
 
 # construct output layer
-dense_layer_3 = Dense(4, activation="sigmoid")(concat_layer)
-output = Dense(1, activation="sigmoid")(dense_layer_3)
+concatenate_dense_layer_1 = Dense(200, activation="sigmoid")(concat_layer)
+concatenate_dense_layer_2 = Dense(200, activation="sigmoid")(concatenate_dense_layer_1)
+output = Dense(1, activation="sigmoid")(concatenate_dense_layer_2)
 
 # build final model
 model = Model(inputs=[input_1, input_2], outputs=output)
@@ -41,11 +44,24 @@ model = Model(inputs=[input_1, input_2], outputs=output)
 model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=['acc'])
 
 # plot the model
-plot_model(model, to_file='../nn_plots/lstm_plus_user_attr/model_plot.png', show_shapes=True, show_layer_names=True)
+# plot_model(model, to_file='../nn_plots/lstm_plus_user_attr/model_plot.png', show_shapes=True, show_layer_names=True)
 
 # train the model
-model.fit(x=[data.X_train_text, data.X_train_user], y=data.y_train, epochs=clf_config.EPOCHS, validation_split=clf_config.VALIDATION_SIZE)
+history = model.fit(x=[data.X_train_text, data.X_train_user], verbose=0, y=data.y_train, epochs=clf_config.EPOCHS, validation_split=clf_config.VALIDATION_SIZE)
+
+epochs = range(1, clf_config.EPOCHS+1)
+
+# ACCURACY
+# plt.plot(epochs, history.history['acc'])
+# plt.plot(epochs, history.history['val_acc'])
+# plt.title('Training and Validation Accuracy')
+# plt.ylabel('accuracy')
+# plt.xlabel('epochs')
+# plt.legend(['train', 'val'], loc='lower right')
+# plt.savefig('cnn_plus_user_accuracy.png')
+
 
 # evaluate the model on the test dataset
 loss, accuracy = model.evaluate(x=[data.X_test_text, data.X_test_user], y=data.y_test, verbose=0)
 print('Accuracy: %f' % (accuracy*100))
+print('Loss: %f' % (loss*100))
